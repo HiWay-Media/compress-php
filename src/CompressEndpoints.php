177e02176f75e22e3bee02b51552c206c6973550
Allan Nava <allannava95@gmail.com>
2023-11-20 10:06:58 +0100

    ADDED COMPRESSENDPOINTS

---
+++ src/CompressEndpoints.php
@@ -0,0 +1,23 @@
+<? php
+//
+const COMPRESS_API_URL       = "https://api.tngrm.io";    
+//
+class COMPRESS_ENDPOINTS {
+    // Added tangram endoints
+    const CATEGORIES_URL                = COMPRESS_API_URL."/api/v3.0/external/upload/categories"; // /create
+    const CREATE_CATEGORY               = COMPRESS_ENDPOINTS::CATEGORIES_URL."/create";
+    //
+    const GET_RESTREAMERS               = COMPRESS_API_URL."/api/v3.0/external/restreamers";
+    const GET_RUNNING_INSTANCES         = COMPRESS_ENDPOINTS::GET_RESTREAMERS."/running_instances";
+    const GET_RUNNING_SINGLE_INSTANCE   = COMPRESS_ENDPOINTS::GET_RESTREAMERS."/single_instance";
+    const GET_UPLOADS                   = COMPRESS_API_URL."/api/v3.0/external/upload";
+    const GET_PUBLISHED_UPLOADS         = COMPRESS_ENDPOINTS::GET_UPLOADS . "/get_published";
+    const PUBLISH_UPLOAD                = COMPRESS_ENDPOINTS::GET_UPLOADS ."/set_published";
+    const CREATE_UPLOAD                 = COMPRESS_ENDPOINTS::GET_UPLOADS ."/create";
+    const PRESIGNED_URL_S3              = COMPRESS_API_URL."/api/v3.0/external/upload/presignedUrl";
+    const ADD_VIDEO_THUMB               = COMPRESS_ENDPOINTS::GET_UPLOADS ."/add_thumb";
+    const S3_SPACE                      = COMPRESS_ENDPOINTS::GET_UPLOADS ."/s3_space";
+    //
+}
+//
+?>
