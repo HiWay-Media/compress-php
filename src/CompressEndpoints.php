1be670e108072c0dc4bded1f76768dd0236a0083
Allan Nava <allannava95@gmail.com>
2024-04-24 15:04:34 +0200

    Update CompressEndpoints.php
    refactor v4.0 with new endpoint

--- src/CompressEndpoints.php
+++ src/CompressEndpoints.php
@@ -1,20 +1,20 @@
 <? php
 //
-const COMPRESS_API_URL       = "https://api.tngrm.io";    
+const COMPRESS_API_URL       = "https://api-compress.hiway.media";    
 //
 class COMPRESS_ENDPOINTS {
     // Added tangram endoints
-    const CATEGORIES_URL                = COMPRESS_API_URL."/api/v3.0/external/upload/categories"; // /create
+    const CATEGORIES_URL                = COMPRESS_API_URL."/api/v4.0/external/upload/categories"; // /create
     const CREATE_CATEGORY               = COMPRESS_ENDPOINTS::CATEGORIES_URL."/create";
     //
-    const GET_RESTREAMERS               = COMPRESS_API_URL."/api/v3.0/external/restreamers";
+    const GET_RESTREAMERS               = COMPRESS_API_URL."/api/v4.0/external/restreamers";
     const GET_RUNNING_INSTANCES         = COMPRESS_ENDPOINTS::GET_RESTREAMERS."/running_instances";
     const GET_RUNNING_SINGLE_INSTANCE   = COMPRESS_ENDPOINTS::GET_RESTREAMERS."/single_instance";
-    const GET_UPLOADS                   = COMPRESS_API_URL."/api/v3.0/external/upload";
+    const GET_UPLOADS                   = COMPRESS_API_URL."/api/v4.0/external/upload";
     const GET_PUBLISHED_UPLOADS         = COMPRESS_ENDPOINTS::GET_UPLOADS . "/get_published";
     const PUBLISH_UPLOAD                = COMPRESS_ENDPOINTS::GET_UPLOADS ."/set_published";
     const CREATE_UPLOAD                 = COMPRESS_ENDPOINTS::GET_UPLOADS ."/create";
-    const PRESIGNED_URL_S3              = COMPRESS_API_URL."/api/v3.0/external/upload/presignedUrl";
+    const PRESIGNED_URL_S3              = COMPRESS_API_URL."/api/v4.0/external/upload/presignedUrl";
     const ADD_VIDEO_THUMB               = COMPRESS_ENDPOINTS::GET_UPLOADS ."/add_thumb";
     const S3_SPACE                      = COMPRESS_ENDPOINTS::GET_UPLOADS ."/s3_space";
     //
