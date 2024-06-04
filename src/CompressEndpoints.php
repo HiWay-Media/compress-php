56a4a3b770bf87e485561f59ae0b1f86c261184b
Allan Nava <allannava95@gmail.com>
2024-06-04 16:11:38 +0200

    added new urls

--- src/CompressEndpoints.php
+++ src/CompressEndpoints.php
@@ -6,17 +6,25 @@ class COMPRESS_ENDPOINTS {
     // Added tangram endoints
     const CATEGORIES_URL                = COMPRESS_API_URL."/api/v4.0/external/upload/categories"; // /create
     const CREATE_CATEGORY               = COMPRESS_ENDPOINTS::CATEGORIES_URL."/create";
-    //
+    // Restreamers
     const GET_RESTREAMERS               = COMPRESS_API_URL."/api/v4.0/external/restreamers";
     const GET_RUNNING_INSTANCES         = COMPRESS_ENDPOINTS::GET_RESTREAMERS."/running_instances";
     const GET_RUNNING_SINGLE_INSTANCE   = COMPRESS_ENDPOINTS::GET_RESTREAMERS."/single_instance";
+    # Uploads
     const GET_UPLOADS                   = COMPRESS_API_URL."/api/v4.0/external/upload";
     const GET_PUBLISHED_UPLOADS         = COMPRESS_ENDPOINTS::GET_UPLOADS . "/get_published";
     const PUBLISH_UPLOAD                = COMPRESS_ENDPOINTS::GET_UPLOADS ."/set_published";
     const CREATE_UPLOAD                 = COMPRESS_ENDPOINTS::GET_UPLOADS ."/create";
-    const PRESIGNED_URL_S3              = COMPRESS_API_URL."/api/v4.0/external/upload/presignedUrl";
+    const PRESIGNED_URL_S3              = COMPRESS_API_URL::"/api/v4.0/external/upload/presignedUrl";
     const ADD_VIDEO_THUMB               = COMPRESS_ENDPOINTS::GET_UPLOADS ."/add_thumb";
     const S3_SPACE                      = COMPRESS_ENDPOINTS::GET_UPLOADS ."/s3_space";
+    //
+    # Customers
+    const CUSTOMERS                      = COMPRESS_API_URL . "/api/v4.0/external/customers";
+    const GET_CUSTOMER_ZONE              = COMPRESS_ENDPOINTS::CUSTOMERS . "/s3";
+    # Events 
+    const EVENTS_EXTERNAL                = COMPRESS_API_URL. "/api/v4.0/external/events";
+    const BULK_EVENTS_CREATE             = COMPRESS_ENDPOINTS::EVENTS_EXTERNAL . "/create_bulk";
     //
 }
 //
