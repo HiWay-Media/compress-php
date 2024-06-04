d6dc0ca989d46d596588fa0eb0e3ccd44c8f038a
Allan Nava <allannava95@gmail.com>
2024-06-04 16:13:58 +0200

    added new urls

--- src/CompressEndpoints.php
+++ src/CompressEndpoints.php
@@ -8,16 +8,23 @@ class COMPRESS_ENDPOINTS {
     const CREATE_CATEGORY               = COMPRESS_ENDPOINTS::CATEGORIES_URL."/create";
     // Restreamers
     const GET_RESTREAMERS               = COMPRESS_API_URL."/api/v4.0/external/restreamers";
-    const GET_RUNNING_INSTANCES         = COMPRESS_ENDPOINTS::GET_RESTREAMERS."/running_instances";
-    const GET_RUNNING_SINGLE_INSTANCE   = COMPRESS_ENDPOINTS::GET_RESTREAMERS."/single_instance";
+    const GET_RUNNING_INSTANCES         = COMPRESS_ENDPOINTS::GET_RESTREAMERS . "/running_instances";
+    const GET_RUNNING_SINGLE_INSTANCE   = COMPRESS_ENDPOINTS::GET_RESTREAMERS . "/single_instance";
+    const SCALE_RESTREAMER              = COMPRESS_ENDPOINTS::GET_RESTREAMERS . "/scale_instance";
+    const RESTREAMER_HLS_START          = COMPRESS_ENDPOINTS::GET_RESTREAMERS . "/hls/start";
+    const RESTREAMER_HLS_STOP           = COMPRESS_ENDPOINTS::GET_RESTREAMERS . "/hls/stop";
+    const RESTREAMER_PUSH_START         = COMPRESS_ENDPOINTS::GET_RESTREAMERS . "/push/start";
+    const RESTREAMER_PUSH_STOP          = COMPRESS_ENDPOINTS::GET_RESTREAMERS . "/push/stop";
+    const RESTREAMER_PULL_START         = COMPRESS_ENDPOINTS::GET_RESTREAMERS . "/pull/start";
+    const RESTREAMER_PULL_STOP          = COMPRESS_ENDPOINTS::GET_RESTREAMERS . "/pull/stop";
     # Uploads
     const GET_UPLOADS                   = COMPRESS_API_URL."/api/v4.0/external/upload";
     const GET_PUBLISHED_UPLOADS         = COMPRESS_ENDPOINTS::GET_UPLOADS . "/get_published";
-    const PUBLISH_UPLOAD                = COMPRESS_ENDPOINTS::GET_UPLOADS ."/set_published";
-    const CREATE_UPLOAD                 = COMPRESS_ENDPOINTS::GET_UPLOADS ."/create";
-    const PRESIGNED_URL_S3              = COMPRESS_API_URL::"/api/v4.0/external/upload/presignedUrl";
-    const ADD_VIDEO_THUMB               = COMPRESS_ENDPOINTS::GET_UPLOADS ."/add_thumb";
-    const S3_SPACE                      = COMPRESS_ENDPOINTS::GET_UPLOADS ."/s3_space";
+    const PUBLISH_UPLOAD                = COMPRESS_ENDPOINTS::GET_UPLOADS . "/set_published";
+    const CREATE_UPLOAD                 = COMPRESS_ENDPOINTS::GET_UPLOADS . "/create";
+    const PRESIGNED_URL_S3              = COMPRESS_API_URL . "/api/v4.0/external/upload/presignedUrl";
+    const ADD_VIDEO_THUMB               = COMPRESS_ENDPOINTS::GET_UPLOADS . "/add_thumb";
+    const S3_SPACE                      = COMPRESS_ENDPOINTS::GET_UPLOADS . "/s3_space";
     //
     # Customers
     const CUSTOMERS                      = COMPRESS_API_URL . "/api/v4.0/external/customers";
